8b94cbcc12ca1f00bc29c189b41e9686724a876b
khansajiullah6-lab <khansajiullah6@gmail.com>
2026-06-08 19:01:40 +0530

    Update safety_tbt_ai.py

--- safety_tbt_ai.py
+++ safety_tbt_ai.py
@@ -23,7 +23,7 @@ else:
     # Configure Gemini
     genai.configure(api_key=api_key)
     model = genai.GenerativeModel('gemini-1.5-flash')
-
+model = genai.GenerativeModel('gemini-pro')
     # Sidebar Options
     str.sidebar.title("🛠️ Customization")
     language = str.sidebar.selectbox("Language Select Karein:", ["Hinglish", "Hindi", "English"])
